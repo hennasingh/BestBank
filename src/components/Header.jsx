1d76a19a5a7410436e31b9f88b653f6ca0642d26
Henna Singh <hennasingh04@gmail.com>
2024-02-03 22:01:46 +0000

    implemented home component and related styles. displayed result on click of the account

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,18 +4,18 @@ import logo from '../assets/bestBankLogo.png'
 export default function Header() {
 
     return (
-        <>
-        <img className='logo' src={logo} alt='logo of BestBank, mono pyramid'/>
-        <nav>
-            <ul className='nav-list'>
-                <li>Home</li>
-                <li>Payments</li>
-                <li>Savings</li>
-                <li>Financing</li>
-                <li>Stocks</li>
-            </ul>
+        <header>
+            <img className='logo' src={logo} alt='logo of BestBank, mono pyramid'/>
+            <nav>
+                <ul className='nav-list'>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Payments</a></li>
+                    <li><a href="#">Savings</a></li>
+                    <li><a href="#">Financing</a></li>
+                    <li><a href="#">Stocks</a></li>
+                </ul>
 
-        </nav>
-        </>
+            </nav>
+        </header>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,16 +3,72 @@ import { getAccounts } from '../api'
 
 export default function Home() {
 
-    getAccounts()
+    const [accounts, setAccounts] = React.useState([])
+    const [clickedAccount , setClickedAccount] = React.useState(1)
+    const [accountSpendings, setAccountSpendings] = React.useState([])
+
+   React.useEffect(() => {
+        async function loadAccounts() {
+            try {
+                const data = await getAccounts()
+                setAccounts(data)
+            
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        loadAccounts()
+   }, [])
+
+   const accountElements = accounts.map(account => (        
+        
+        <li className={`account ${clickedAccount == account.id ? 'selected': ''}`} key={account.id} onClick={() => displaySpendings(account.id)}>
+            <p>{account.title} </p>
+            <p>$ {account.balance}</p>
+        </li> 
+   ))
+
+   const displaySpendings = (accountId) => {
+        setClickedAccount(accountId)
+        const account = accounts.filter(account => account.id == accountId)[0]
+        setAccountSpendings(account.spendings)
+   }
 
 
     return (
-        <div>
-            <div>
-                <button className="action-btn">Pay</button>
-                <button className="action-btn">Transfer</button>
-            </div>
-        </div>
+        <main>
+            <section>
+                <div className="buttons">
+                    <button className="action-btn">Pay</button>
+                    <button className="action-btn">Transfer</button>
+                </div>
+            </section>
+            <section className="section-accounts">
+                <div className="container">
+                    <div className='account-list'>
+                        <h2>Accounts</h2>
+                        <ul>
+                            { accountElements}
+                        </ul>
+                    </div>
+                    <div className="div-spendings">
+                        <h2>Spendings</h2>
+                        <div className="spending-list">
+                            {
+                                accountSpendings.map((spending, index) =>(
+                                    <div className="progress-container" key={index}>
+                                        <div className='progress-bar'>
+                                            <p>{spending.category}</p>
+                                            <p>$ {spending.spent}</p>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     )
 
 }
